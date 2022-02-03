8060f05759ac092eac18b6736be109a4c3f7c1cb
jcvisk <juan.carlos.gcn@gmail.com>
2022-02-03 09:47:31 -0600

    agregando metatags

---
+++ js/front.js
@@ -0,0 +1,9 @@
+    /* JS PARA QUE LOS METATAGAS SEAN DIANMICOS*/
+    //Asignando dominio a metaTags
+    var dominio = $(location).attr('hostname');
+    $('#metatagUrlFacebook').attr('content', dominio)
+    $('#metatagUrlTwitter').attr('content', dominio)
+    //Asignando direccion de favicon a metaTags
+    dominio += "/images/favicon.ico"
+    $('#metatagFaviconFacebook').attr('content', dominio)
+    $('#metatagFaviconTwitter').attr('content', dominio)
