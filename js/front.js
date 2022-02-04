7e91dcf975d5a54176bab8525135df510e636584
jcvisk <juan.carlos.gcn@gmail.com>
2022-02-04 08:20:42 -0600

    corrigiendo politicas

--- js/front.js
+++ js/front.js
@@ -1,9 +1,10 @@
-    /* JS PARA QUE LOS METATAGAS SEAN DIANMICOS*/
-    //Asignando dominio a metaTags
-    var dominio = $(location).attr('hostname');
-    $('#metatagUrlFacebook').attr('content', dominio)
-    $('#metatagUrlTwitter').attr('content', dominio)
-    //Asignando direccion de favicon a metaTags
-    dominio += "/images/favicon.ico"
-    $('#metatagFaviconFacebook').attr('content', dominio)
-    $('#metatagFaviconTwitter').attr('content', dominio)
+/* JS PARA QUE LOS METATAGAS SEAN DIANMICOS*/
+//Asignando dominio a metaTags
+
+var dominio = $(location).attr('hostname');
+$('#metatagUrlFacebook').attr('content', dominio)
+$('#metatagUrlTwitter').attr('content', dominio)
+//Asignando direccion de favicon a metaTags
+dominio += "/images/favicon.ico"
+$('#metatagFaviconFacebook').attr('content', dominio)
+$('#metatagFaviconTwitter').attr('content', dominio)
